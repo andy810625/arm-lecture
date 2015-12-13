6f5caf0410627245267e97047ab9da9cc39e3298
andy810625 <andy810625@hotmail.com>
2015-12-13 13:56:10 +0800

    implement tail recursion
    Use this code to modify to assembly.
    
    int fib (int term, int val, int prev)
    {
        if(term == 0) return prev;
        if(term == 1) return val;
        return fib(term-1, val+prev, val);
    }

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,48 +11,39 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r4, r5, lr}
+	push {r3, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-    
-    subs r4, r0, #0
-    ble .L3
-    
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-    
-    cmp r4, 1
+        @ if(term == 0) return prev
+
+    cmp r0, #0
+    beq .L3
+
+        @ if(term == 1) return val
+
+    cmp r0, #1
     beq .L4
-    
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-    
-    add r0, r4, #4294967295
+
+        @ calculate term-1, val+prev, val
+
+    sub r0, r0, #1
+    add r3, r1, r2
+    mov r2, r1
+    mov r1, r3
+
+	@return fib(term-1, val+prev, val)
+   
     bl fibonacci
     
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-    
-    mov r5, r0
-    sub r0, r4, #2
-    bl fibonacci
-    
-	@ R0 = R5 + R0 (update flags)
-    
-    adds r0, r5, r0
-    
-	pop {r4, r5, pc}		@EPILOG
+	pop {r3, pc}			@ EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r4, r5, pc}		@ EPILOG
+	mov r0, r2			@ R0 = 0
+	pop {r3, pc}			@ EPILOG
 
 .L4:
-	mov r0, #1			@ R0 = 1
-	pop {r4, r5, pc}		@ EPILOG
+	mov r0, r1			@ R0 = 1
+	pop {r3, pc}			@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
